0a008ee198073ec55153fc235c28b60c50af41ec
Daaaaaav <scarletstormsubs@gmail.com>
2025-06-20 14:09:05 +0700

    hime

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { increment, decrement } from './store/counter/action';
 import './App.css'
 import { store } from './store';
 import { Provider } from 'react-redux';
+import hime from './assets/hime.jpg';
 
 function App() {
   const count = useSelector(state => state.count)
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <Provider store={store}>
+      <img src={hime} alt="Himes"/>
       <h1>Count: {count}</h1>
       <button onClick={()=>dispatch(increment())}>+</button>
       <button onClick={()=>dispatch(decrement())}>-</button>
